f72e5efae1dc4fd1b9e282f216f8855658e07ff2
Raman Fedaseyeu <werediver@pm.me>
2023-12-25 21:07:05 +0100

    Use Chroma DB to persist embeddings

--- src/app.py
+++ src/app.py
@@ -1,13 +1,23 @@
-from os import getenv
+import os
+import time
 
-from llama_index import PromptTemplate, VectorStoreIndex, SimpleDirectoryReader, ServiceContext
+from llama_index import PromptTemplate, VectorStoreIndex, SimpleDirectoryReader
+from llama_index.service_context import ServiceContext, set_global_service_context
+from llama_index.storage.storage_context import StorageContext
+# from llama_index.vector_stores.simple import SimpleVectorStore
+from llama_index.vector_stores import ChromaVectorStore
+from llama_index.vector_stores.types import (
+    DEFAULT_PERSIST_DIR,
+    DEFAULT_PERSIST_FNAME,
+)
 from llama_index.node_parser import SimpleNodeParser
 from llama_index.llms import LLM, Ollama
 from llama_index.embeddings import OllamaEmbedding
-from llama_index.service_context import set_global_service_context
 
 from llama_index.query_engine import CustomQueryEngine
 from llama_index.retrievers import BaseRetriever
+
+import chromadb
 
 class RAGQueryEngine(CustomQueryEngine):
     retriever: BaseRetriever
@@ -20,7 +30,24 @@ class RAGQueryEngine(CustomQueryEngine):
         query = self.prompt_template.format(context_str=context_str, query_str=query_str)
         print(f"Query: {query}") # Debug
         return self.llm.complete(query, formatted=False)
-    
+
+def mark(prefix: str, suffix: str | None = None):
+  class Mark:
+    def __init__(self, prefix: str, suffix: str):
+        self.prefix = prefix
+        self.suffix = suffix
+        self.t = 0.0
+
+    def __enter__(self):
+      print(self.prefix, end="")
+      self.t = time.perf_counter()
+
+    def __exit__(self, exc_type, exc_value, exc_tb):
+      elapsed = time.perf_counter() - self.t
+      print(self.suffix.format(elapsed))
+
+  return Mark(prefix, suffix or "{:.2f} s")
+
 def main():
   # TODO: If only one model is available via the API, detect and select it automatically.
   model_id = "mistral"
@@ -31,15 +58,32 @@ def main():
   )
   # set_global_service_context(service_ctx)
 
-  docs_path = getenv("DOCS")
-  assert not docs_path is None, "Environment variable DOCS must point to a directory with documents to index"
-  documents = SimpleDirectoryReader(docs_path, required_exts=[".md"]).load_data()
+  chroma_client = chromadb.PersistentClient()
+  chroma_collection = chroma_client.get_or_create_collection("context")
+  vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
 
-  node_parser = SimpleNodeParser.from_defaults(chunk_size=512)
-  nodes = node_parser.get_nodes_from_documents(documents)
-  vector_index = VectorStoreIndex(nodes, service_context=service_ctx) # Requires an embedding model
+  storage_ctx = StorageContext.from_defaults(vector_store=vector_store)
 
-  qa_prompt = PromptTemplate(
+  if chroma_collection.count() == 0:
+    with mark("Scanning documents... "):
+      docs_path = os.getenv("DOCS")
+      assert docs_path is not None, "Environment variable DOCS must point to a directory with documents to index"
+      documents = SimpleDirectoryReader(docs_path, required_exts=[".md"]).load_data()
+
+      node_parser = SimpleNodeParser.from_defaults(chunk_size=768, include_metadata=False)
+      nodes = node_parser.get_nodes_from_documents(documents)
+  else:
+     nodes = []
+
+  # `VectorStoreIndex` requires an embedding model in `service_context`.
+  vector_index = VectorStoreIndex(
+    nodes, 
+    service_context=service_ctx, 
+    storage_context=storage_ctx,
+    show_progress=True,
+  )
+
+  prompt_template = PromptTemplate(
     "Below are pieces of the context information followed by the text \"End of context.\"\n\n"
     "{context_str}\n\n"
     "End of context.\n\n"
@@ -50,13 +94,13 @@ def main():
   query_engine = RAGQueryEngine(
      llm=service_ctx.llm, 
      retriever=vector_index.as_retriever(), 
-     prompt_template=qa_prompt
+     prompt_template=prompt_template
   )
 
   # A non-augmented request (comment out the docs indexing code to speed up execution)
-  response = service_ctx.llm.complete("What is the moon?", formatted=False)
+  # response = service_ctx.llm.complete("What is the moon?", formatted=False)
 
-  # response = query_engine.query("Which NAS cases are mentioned in the context?")
+  response = query_engine.query("What can AWG mean?")
 
   print(f"Response: {response}")
 
